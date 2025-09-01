6247185ec7aa4174c19a2bda81f66871a896060e
Adam <adamaettinata@gmail.com>
2025-09-01 14:38:52 +0700

    dynamic deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js'); // Import Supabase
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // --- KONEKSI SUPABASE ---
 const supabaseUrl = process.env.SUPABASE_URL;
